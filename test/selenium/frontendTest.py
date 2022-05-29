02a6edf7bbc724038bb2a5254214236529fc27c5
PandaAcademy <kontakt@pandait.pl>
2022-05-29 15:29:21 +0000

    Complete CD/CD pipeline

--- test/selenium/frontendTest.py
+++ test/selenium/frontendTest.py
@@ -8,25 +8,25 @@ application_URL = environ.get('APPLICATION_URL', 'http://192.168.44.44:5000/')
 selenium_URL = environ.get('SELENIUM_URL', 'http://192.168.44.44:4444/wd/hub')
 
 
-class PythonOrgSearch(unittest.TestCase):
-
+class AppTest(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Firefox()
-        # self.driver = webdriver.Remote(
-        #     command_executor=selenium_URL,
-        #     options=webdriver.FirefoxOptions())
+        # self.driver = webdriver.Firefox()
+        self.driver = webdriver.Remote(
+            command_executor=selenium_URL,
+            options=webdriver.FirefoxOptions())
 
-    def test_search_in_python_org(self):
+    def test_our_app(self):
         driver = self.driver
-        driver.implicitly_wait(5)
-        driver.get("http://www.python.org")
-        self.assertIn("Python", driver.title)
-        elem = driver.find_element(By.NAME, "q")
-        elem.send_keys("pycon")
+        driver.set_page_load_timeout(5)
+        driver.set_script_timeout(5)
+        driver.get(application_URL)
+        elem = driver.find_element(By.NAME, "name")
+        elem.send_keys("luka")
+        elem = driver.find_element(By.NAME, "animal")
+        elem.send_keys("dog")
         elem.send_keys(Keys.RETURN)
         self.assertNotIn("No results found.", driver.page_source)
         driver.quit()
-
 
 if __name__ == "__main__":
     unittest.main()
